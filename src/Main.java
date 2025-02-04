2cc6e30f8b210a75344811a71b9a1323f85cd3f0
KelseyF624 <kelseyf.624@gmail.com>
2025-02-04 16:31:19 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int rock = 0;
+        int paper = 1;
+        int scissors = 2;
+        System.out.println("Welcome to Rock Paper Scissors! Please enter your choice Player A: 0=Rock, 1=Paper or 2=Scissors!");
+        
+    }
+}
