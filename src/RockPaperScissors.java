4b509f34dcd3f7f0d108dc5c6abdce21185e0f10
KelseyF624 <kelseyf.624@gmail.com>
2025-02-04 22:45:25 -0500

    Partial Commit

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -2,15 +2,24 @@ import java.util.Scanner;
 
 public class RockPaperScissors {
     public static void main(String[] args) {
-        int rock = 0;
-        int paper = 1;
-        int scissors = 2;
-        System.out.println("Welcome to Rock Paper Scissors! Please enter your choice Player A: 0=Rock, 1=Paper or 2=Scissors!");
-        Scanner sc = new Scanner(System.in);
-        int playerAChoice = sc.nextInt();
-        System.out.println("Player B please enter your choice:");
-        int playerBChoice = sc.nextInt();
-        if (playerAChoice == playerBChoice) {System.out.println("It's a TIE!");}
-
+        String RPSMove = "";
+        String Replay = "R";
+        do {
+            System.out.println("Welcome to Rock Paper Scissors! Please enter your choice Player A: R, P or S!");
+            Scanner sc = new Scanner(System.in);
+            String playerAChoice = sc.nextLine();
+            System.out.println("Player B please enter your choice:");
+            String playerBChoice = sc.nextLine();
+            if (playerAChoice == playerBChoice) {
+                System.out.println("It's a TIE!");
+            } else if (playerAChoice.equals("R")) && playerBChoice.equals("S")) {
+                System.out.println("Player A WINS!");
+            } else if (playerAChoice.equals("S") && playerBChoice.equals("P")) {
+                System.out.println("Player A WINS!");}
+            else if (playerAChoice.equals("P") && playerBChoice.equals("R"))
+            {System.out.println("Player A WINS!");}
+            else if (System.out.println("Player B WINS!");
+            System.out.println("Would you like to play again? (Y/N)");
+        while (!Replay.equals("Y") && !Replay.equals("N")) {System.out.println("Would you like to play again?");}
     }
-}
+}}
