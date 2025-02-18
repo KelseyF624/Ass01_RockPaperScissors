5c763f00dc7521e180a6478c1b5fb96afeae9812
KelseyF624 <kelseyf.624@gmail.com>
2025-02-18 16:35:57 -0500

    Partial Commit

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -1,54 +1,51 @@
+import java.util.Objects;
 import java.util.Scanner;
 public class RockPaperScissors {
     public static void main(String[] args) {
         //rock = 1
         //paper = 2
         //scissors = 3
-        int playerAChoice;
-        int playerBChoice;
-        int replayChoice;
+        String playerAChoice;
+        String playerBChoice;
+        String replayChoice;
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Rock Paper Scissors!");
         do {
             do {
-                System.out.println("Please enter your choice Player A: 1=Rock, 2=Paper, 3=Scissors!");
-                playerAChoice = sc.nextInt();
-                if (playerAChoice <= 0) {
+                System.out.println("Please enter your choice Player A: R, P,S.");
+                playerAChoice = sc.nextLine();
+                if (playerAChoice != null) {
                     System.out.println("Please enter a valid choice!");}
-                else if (playerAChoice >= 4) {
-                    System.out.println("Please enter a valid choice!");}
-            } while (playerAChoice <= 0 || playerAChoice >= 4);
+            } while (Objects.equals(playerAChoice, "R")); || (Objects.equals(playerAChoice, "P")) || (Objects.equals(playerAChoice, "S"));
             do {
                 System.out.println("Player B please enter your choice:");
-                playerBChoice = sc.nextInt();
-                if (playerBChoice <= 0) {
+                playerBChoice = sc.nextLine();
+                if (playerBChoice != null) {
                     System.out.println("Please enter a valid choice!");}
-                else if (playerBChoice >= 4) {
-                    System.out.println("Please enter a valid choice!");}
-            } while (playerBChoice <= 0 || playerBChoice >= 4);
+            } while (Objects.equals(playerBChoice, "R")) || Objects.equals(playerBChoice, "P")) || (Objects.equals(playerBChoice, "S"));
             System.out.println("Player A picked: " + playerAChoice);
             System.out.println("Player B picked: " + playerBChoice);
-            if (playerAChoice == 1 && playerBChoice == 1) {
+            if (Objects.equals(playerAChoice, "R") && (Objects.equals(playerBChoice, "R")) {
                 System.out.println("Rock vs. Rock! It's a...TIE!");}
-            else if (playerAChoice == 2 && playerBChoice == 2) {
+            else if (Objects.equals(playerAChoice, "P") && (Objects.equals(playerBChoice, "P")) {
                 System.out.println("Paper vs. Paper! It's a...TIE!");}
-            else if (playerAChoice == 3 && playerBChoice == 3) {
+            else if (Objects.equals(playerAChoice, "S") && Objects.equals(playerBChoice, "S")) {
                 System.out.println("Scissors vs Scissors! It's a...TIE!");}
-            else if (playerAChoice == 2 && playerBChoice == 1) {
+            else if (Objects.equals(playerAChoice, "P") && Objects.equals(playerBChoice, "S")) {
                 System.out.println("Paper beats Scissors! Player A...WINS!");}
-            else if (playerAChoice == 3 && playerBChoice == 2) {
+            else if (Objects.equals(playerAChoice, "S") && (Objects.equals(playerBChoice, "P") {
                 System.out.println("Scissors beats Paper! Player A...WINS!");}
-            else if (playerAChoice == 1 && playerBChoice == 3) {
+            else if (Objects.equals(playerAChoice, "R") && Objects.equals(playerBChoice, "S")) {
                 System.out.println("Rock beats Scissors! Player A...WINS!");}
-            else if (playerAChoice == 1 && playerBChoice == 2) {
+            else if (Objects.equals(playerAChoice, "P")) && (Objects.equals(playerBChoice, "S")) {
                 System.out.println("Paper beats Scissors! Player B...WINS!");}
-            else if (playerAChoice == 2 && playerBChoice == 3) {
+            else if (Objects.equals(playerAChoice, "S") && Objects.equals(playerBChoice, "P")) {
                 System.out.println("Scissors beats Paper! Player B...WINS!");}
-            else if (playerAChoice == 3 && playerBChoice == 1) {
+            else if (Objects.equals(playerAChoice, "R") && Objects.equals(playerBChoice, "S")) {
                 System.out.println("Rock beats Scissors! Player B...WINS!");}
-            System.out.println("Would you like to play again? (0 to replay. 4 to terminate.)");
-            replayChoice = sc.nextInt();
-            if (replayChoice== 4) {System.out.println("Thank you for playing!");}
-        }while (replayChoice== 0);
+            System.out.println("Would you like to play again? (Y/N)");
+            replayChoice = sc.nextLine();
+            if (replayChoice.equals("N")) {System.out.println("Thank you for playing!");}
+        }while (replayChoice.equals("Y"));
     }
 }
