64a9e1ed98a8da7612331f3ca850005a08793e7f
KelseyF624 <kelseyf.624@gmail.com>
2025-02-18 20:52:58 -0500

    Partial Commit

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -2,50 +2,48 @@ import java.util.Objects;
 import java.util.Scanner;
 public class RockPaperScissors {
     public static void main(String[] args) {
-        //rock = 1
-        //paper = 2
-        //scissors = 3
-        String playerAChoice;
-        String playerBChoice;
-        String replayChoice;
+        String playerAChoice="R, P, S";
+        String playerBChoice="R, P, S";
+        String ContinueGame ="Y";
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Rock Paper Scissors!");
         do {
             do {
-                System.out.println("Please enter your choice Player A: R, P,S.");
+                System.out.println("Player A, please enter your choice: (R, P, S)");
                 playerAChoice = sc.nextLine();
-                if (playerAChoice != null) {
-                    System.out.println("Please enter a valid choice!");}
-            } while (Objects.equals(playerAChoice, "R")); Objects.equals(playerAChoice, "P"); Objects.equals(playerAChoice, "S");
+                if (playerAChoice.equalsIgnoreCase("R") || (playerAChoice.equalsIgnoreCase("P")) || (playerAChoice.equalsIgnoreCase("S"))) {
+                    System.out.println("Player A choice confirmed.");}
+            } while (playerAChoice.equalsIgnoreCase(""));{
+                System.out.println("Invalid choice.");}
             do {
-                System.out.println("Player B please enter your choice:");
+                System.out.println("Player B, please enter your choice: (R, P, S)");
                 playerBChoice = sc.nextLine();
-                if (playerBChoice != null) {
-                    System.out.println("Please enter a valid choice!");}
-            } while (Objects.equals(playerBChoice, "R")); Objects.equals(playerBChoice, "P"); Objects.equals(playerBChoice, "S");
-            System.out.println("Player A picked: " + playerAChoice);
-            System.out.println("Player B picked: " + playerBChoice);
-            if (Objects.equals(playerAChoice, "R") && (Objects.equals(playerBChoice, "R"))) {
-                System.out.println("Rock vs. Rock! It's a...TIE!");}
-            else if (Objects.equals(playerAChoice, "P") && (Objects.equals(playerBChoice, "P"))) {
-                System.out.println("Paper vs. Paper! It's a...TIE!");}
-            else if (Objects.equals(playerAChoice, "S") && Objects.equals(playerBChoice, "S")) {
-                System.out.println("Scissors vs Scissors! It's a...TIE!");}
-            else if (Objects.equals(playerAChoice, "P") && Objects.equals(playerBChoice, "S")) {
-                System.out.println("Paper beats Scissors! Player A...WINS!");}
-            else if (Objects.equals(playerAChoice, "S") && Objects.equals(playerBChoice, "P")) {
-                System.out.println("Scissors beats Scissors! Player B...WINS!");}
-            else if (Objects.equals(playerAChoice, "R") && Objects.equals(playerBChoice, "S")) {
-                System.out.println("Rock beats Scissors! Player A...WINS!");}
-            else if (Objects.equals(playerAChoice, "P") && Objects.equals(playerBChoice, "S")); {
-                System.out.println("Paper beats Scissors! Player B...WINS!");}
-            else if (Objects.equals(playerAChoice, "S") && Objects.equals(playerBChoice, "P")) {
-                System.out.println("Scissors beats Paper! Player B...WINS!");}
-            else if (Objects.equals(playerAChoice, "R") && Objects.equals(playerBChoice, "S")) {
-                System.out.println("Rock beats Scissors! Player B...WINS!");}
-            System.out.println("Would you like to play again? (Y/N)");
-            replayChoice = sc.nextLine();
-            if (replayChoice.equals("N")) {System.out.println("Thank you for playing!");}
-        }while (replayChoice.equals("Y"));
+                if (playerBChoice.equalsIgnoreCase("R") || (playerBChoice.equalsIgnoreCase("P")) || (playerBChoice.equalsIgnoreCase("S"))) {
+                    System.out.println("Player B choice confirmed.");}
+            } while (playerBChoice.equalsIgnoreCase(""));{
+                System.out.println("Invalid choice.");}
+            if (playerAChoice.equalsIgnoreCase("R") && (playerBChoice.equalsIgnoreCase("R"))) {
+                System.out.println("Rock vs. Rock! It's a TIE!");}
+            else if (playerAChoice.equalsIgnoreCase("P") && (playerBChoice.equalsIgnoreCase("P"))) {
+                System.out.println("Paper vs. Paper! It's a TIE!");}
+            else if (playerAChoice.equalsIgnoreCase("S") && (playerBChoice.equalsIgnoreCase("S"))) {
+                System.out.println("Scissors vs. Scissors! It's a TIE!");}
+            if (playerAChoice.equalsIgnoreCase("S") && (playerBChoice.equalsIgnoreCase("P"))) {
+                System.out.println("Scissors cuts Paper! Player A...WINS!");}
+            else if (playerAChoice.equalsIgnoreCase("P") && (playerBChoice.equalsIgnoreCase("R"))) {
+                System.out.println("Paper covers Rock! Player A...WINS!");}
+            else if (playerAChoice.equalsIgnoreCase("R") && (playerBChoice.equalsIgnoreCase("S"))) {
+                System.out.println("Rock breaks Scissors! Player A...WINS!");}
+            if (playerAChoice.equalsIgnoreCase("P") && (playerBChoice.equalsIgnoreCase("S"))) {
+                System.out.println("Scissors cut Paper! Player B...WINS!");}
+            else if (playerAChoice.equalsIgnoreCase("R") && (playerBChoice.equalsIgnoreCase("P"))) {
+                System.out.println("Paper covers Rock! Player B...WINS!");}
+            else if (playerAChoice.equalsIgnoreCase("S") && (playerBChoice.equalsIgnoreCase("R"))) {
+                System.out.println("Rock breaks Scissors! Player B...WINS!");}
+            System.out.println ("Play again? (Y/N)");
+            ContinueGame = sc.nextLine();
+            if (ContinueGame.equalsIgnoreCase("N")) {
+                System.out.println("Game Over.");}
+        } while (ContinueGame.equalsIgnoreCase("Y"));
     }
 }
